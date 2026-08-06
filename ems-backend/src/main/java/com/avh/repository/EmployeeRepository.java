eeb2c36fea7779c3aae7358a2328acb0dd01e541
A-B-H-I-J-I-T-H <abhijith4nair@duck.com>
2026-08-06 16:21:07 +0530

    add repository

---
+++ ems-backend/src/main/java/com/avh/repository/EmployeeRepository.java
@@ -0,0 +1,22 @@
+package com.avh.repository;
+
+import com.avh.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    List<Employee> findByEmployeeNameContainingIgnoreCase(String name);
+
+    List<Employee> findByDepartmentIgnoreCase(String department);
+
+    List<Employee> findByDesignationIgnoreCase(String designation);
+
+    List<Employee> findAllByOrderBySalaryAsc();
+
+    List<Employee> findAllByOrderByJoiningDateAsc();
+
+    boolean existsByEmail(String email);
+}
